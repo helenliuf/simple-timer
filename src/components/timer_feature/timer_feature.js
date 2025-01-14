b355614be0826fd17a6d75b6d68d1827ce5895a0
Helen Liu <helenfayliu@gmail.com>
2025-01-13 22:09:28 -0500

    set interval functionality with check icon

--- src/components/timer_feature/timer_feature.js
+++ src/components/timer_feature/timer_feature.js
@@ -10,6 +10,22 @@ export class TimerFeature {
         document.head.appendChild(styleSheet);
     }
 
+    setInterval(){
+        const minutes = document.getElementById("minute-input").value;
+        const seconds = document.getElementById("second-input").value;
+
+        if (minutes && seconds){
+            let totalSeconds = minutes * 60 + seconds;
+            if (totalSeconds == 0){
+                alert("Interval must be more than 0 seconds.");
+                return;
+            } 
+            return totalSeconds;
+        }
+        alert("Please enter a valid number of minutes and/or seconds!")
+        return;
+    }
+
     render() {
         // full container
         const fullContainer = document.createElement("div");
@@ -41,6 +57,11 @@ export class TimerFeature {
         checkIcon.style.cursor = "pointer";
         checkIcon.style.width = "25px";
         checkIcon.style.height = "25px";
+
+        let interval = undefined;
+        checkIcon.addEventListener('click', () => {
+            interval = this.setInterval();
+        });
 
         inputContainer.append(minutesInput);
         inputContainer.append(secondsInput);
